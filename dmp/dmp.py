21375cb8174d5bd25f46f0baa9af01dcfdd3a4e6
Alexander Fabisch <afabisch@googlemail.com>
2013-10-17 00:13:25 +0200

    Simplify code

--- dmp/dmp.py
+++ dmp/dmp.py
@@ -3,19 +3,23 @@ from numpy import *  # This is very ugly, but it is OK for our purposes :)
 class DMP(object):
     def __init__(self, pastor_mod=False):
         self.pastor_mod = pastor_mod
-
-        self.alpha = 25.0 # = D = 20.0
-        self.beta = self.alpha / 4.0 # = K/D = 100.0/20.0 = 5.0
+        # Transformation system
+        self.alpha = 25.0                # = D = 20.0
+        self.beta = self.alpha / 4.0     # = K/D = 100.0/20.0 = 5.0
+        # Canonical system
         self.alpha_t = self.alpha / 3.0
-
+        # Obstacle avoidance
         self.gamma_o = 1000.0
         self.beta_o = 20.0 / pi
 
     def phase(self, tau, n_steps, t):
+        """The phase variable replaces explicit timing. It starts with 1 at the
+        beginning of the movement and converges exponentially to 0.
+        """
         return exp(-self.alpha_t * linspace(0, tau, n_steps)[t] / tau)
 
     def spring_damper(self, x0, g, tau, s, X, Xd):
-        # TODO Muelling's DMP
+        """The transformation system generates a goal-directed movement."""
         if self.pastor_mod:
             # allows smooth adaption to goals, in the original version also the
             # forcing term is multiplied by a constant alpha * beta which you can
@@ -25,6 +29,9 @@ class DMP(object):
             return self.alpha * (self.beta * (g - X) - tau * Xd) / tau**2
 
     def forcing_term(self, x0, g, tau, w, s, X, scale=False):
+        """The forcing term shapes the movement arbitrarily based on the
+        weights.
+        """
         n_features = w.shape[1]
         h = exp(-self.alpha_t * linspace(0, tau, n_features) / tau)
         c = diff(h)
@@ -34,25 +41,23 @@ class DMP(object):
         if scale:
             f *= g - x0
     
-        if len(X.shape) == 3:
+        if X.ndim == 3:
             F = zeros_like(X)
-            F[:, :, 0] = f[0]
-            F[:, :, 1] = f[1]
+            F[:, :] = f
             return F
         else:
             return f
 
     def obstacle(self, o, X, Xd):
+        """Obstacle avoidance is based on point obstacles."""
+        if X.ndim == 1:
+          X = X[newaxis, newaxis, :]
+          Xd = Xd[newaxis, newaxis, :]
+        C = zeros_like(X)
         R = array([[cos(pi/2.0), -sin(pi/2.0)],
                    [sin(pi/2.0),  cos(pi/2.0)]])
-        if len(X.shape) == 3:
-            C = zeros_like(X)
-            for i in xrange(X.shape[0]):
-                for j in xrange(X.shape[1]):
-                    theta = arccos((o-X[i, j]).dot(Xd[i, j]) / (linalg.norm(o-X[i, j]) * linalg.norm(Xd[i, j]) + 1e-10))
-                    C[i, j] = self.gamma_o * R.dot(Xd[i, j]) * theta * exp(-self.beta_o * theta)
-            return C
-        else:
-            theta = arccos((o-X).dot(Xd) / (linalg.norm(o-X) * linalg.norm(Xd) + 1e-10))
-            C = self.gamma_o * R.dot(Xd) * theta * exp(-self.beta_o * theta)
-            return C
+        for i in xrange(X.shape[0]):
+            for j in xrange(X.shape[1]):
+                theta = arccos((o-X[i, j]).dot(Xd[i, j]) / (linalg.norm(o-X[i, j]) * linalg.norm(Xd[i, j]) + 1e-10))
+                C[i, j] = self.gamma_o * R.dot(Xd[i, j]) * theta * exp(-self.beta_o * theta)
+        return squeeze(C)
